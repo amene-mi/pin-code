bec61de8f584abdcccfd028ac521b2dfa4011f0c
Alireza Razinejad <a.razinejad@gmail.com>
2021-09-30 19:10:45 +0330

    refactor: Replace var with const

--- src/scripts/containers/createButton.js
+++ src/scripts/containers/createButton.js
@@ -3,9 +3,9 @@ const getValueNumber = require('../components/getRandomNumber');
 const handleClick = require('./handleClick');
 
 function createBtn() {
-  var numbers = getValueNumber();
-  for (var i = 0; i < numbers.length; i++) {
-    var btn = document.createElement('button');
+  const numbers = getValueNumber();
+  for (let i = 0; i < numbers.length; i++) {
+    const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = numbers[i];
     btn.id = "num#" + numbers[i];
