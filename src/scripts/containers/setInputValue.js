1f4198e7b831f80aa405ae13e5d0739254489178
Alireza Razinejad <a.razinejad@gmail.com>
2021-09-30 19:14:08 +0330

    fix: Remove unnecessarily field

--- src/scripts/containers/setInputValue.js
+++ src/scripts/containers/setInputValue.js
@@ -1,11 +1,10 @@
 const setFocusOnNextInput = require('../components/setFocusOnNextInput');
 
 function setInputValue(inputs, value, focusedInput) {
-    let input = inputs;
     focusedInput.value = value;
     if (value === null) return;
 
-    setFocusOnNextInput(input, focusedInput);
+    setFocusOnNextInput(inputs, focusedInput);
 }
 
 module.exports = setInputValue;
